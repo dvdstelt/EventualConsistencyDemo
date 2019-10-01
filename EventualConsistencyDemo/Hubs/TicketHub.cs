996a7a97e27c97c653da89ab545a547fdd7730f9
mauroservienti <mauro.servienti@gmail.com>
2019-10-01 17:37:45 +0200

    Add message session as a dependency on the TicketHub

--- EventualConsistencyDemo/Hubs/TicketHub.cs
+++ EventualConsistencyDemo/Hubs/TicketHub.cs
@@ -1,13 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
+using NServiceBus;
 
 namespace EventualConsistencyDemo.Hubs
 {
     public class TicketHub : Hub
     {
+        IMessageSession messageSession;
+
+        public TicketHub(IMessageSession messageSession)
+        {
+            this.messageSession = messageSession;
+        }
+
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
