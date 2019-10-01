280d470602476d2dff92d0bcfdf8d3645adb8a70
mauroservienti <mauro.servienti@gmail.com>
2019-10-01 17:26:29 +0200

    remove unused using

--- EventualConsistencyDemo/Startup.cs
+++ EventualConsistencyDemo/Startup.cs
@@ -1,19 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using EventualConsistencyDemo.Hubs;
+﻿using EventualConsistencyDemo.Hubs;
 using EventualConsistencyDemo.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NServiceBus;
-using NServiceBus.Routing;
 using static Shared.Configuration.Configuration;
 
 namespace EventualConsistencyDemo
