a6652cd61fbf9186700d19fb413f2a66737b4e09
Dennis van der Stelt <dvdstelt@gmail.com>
2019-10-08 15:33:27 +0200

    Added reviews

---
+++ EventualConsistencyDemo/Controllers/ReviewsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EventualConsistencyDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Entities;
+
+namespace EventualConsistencyDemo.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        // GET: Reviews
+        public ActionResult Index()
+        {
+            return View(MoviesContext.GetMovies());
+        }
+
+        // GET: Reviews/gameofthrones
+        public ActionResult Movie(string movieurl)
+        {
+            var vm = new ReviewViewModel();
+            vm.Movie = MoviesContext.GetMovies().Single(s => s.UrlTitle == movieurl);
+            vm.Reviews = ReviewsContext.GetReviews().Where(s => s.MovieIdentifier == vm.Movie.Id);
+
+            return View(vm);
+        }
+    }
+}
---
+++ EventualConsistencyDemo/Models/ReviewViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Shared.Entities;
+
+namespace EventualConsistencyDemo.Models
+{
+    public class ReviewViewModel
+    {
+        public Movie Movie { get; set; }
+        public IEnumerable<Review> Reviews { get; set; }
+    }
+}
