0f1e24416f04de52f8ed9d7c34cd9b826d9b9110
Dennis <dvdstelt@gmail.com>
2019-11-15 16:04:27 +0100

    Added new NServiceBus hosting from 7.2

--- EventualConsistencyDemo/Program.cs
+++ EventualConsistencyDemo/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
+using NServiceBus;
+using Shared.Configuration;
 
 namespace EventualConsistencyDemo
 {
@@ -14,11 +17,22 @@ namespace EventualConsistencyDemo
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+            var host = Host.CreateDefaultBuilder(args);
+            host.ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            // Configure NServiceBus
+            host.UseNServiceBus(hostBuilderContext =>
+            {
+                var endpointConfiguration = new EndpointConfiguration("EventualConsistencyDemo");
+                endpointConfiguration.ApplyCommonConfiguration(routingConfig =>
+                {
+                    routingConfig.RouteToEndpoint(typeof(Shared.Commands.SubmitOrder), "server");
+                });
+
+                return endpointConfiguration;
+            });
+
+            host.Build().RunAsync();
+        }
     }
 }
