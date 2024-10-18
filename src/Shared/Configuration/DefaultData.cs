978e29802134007209a9080677d8d7c895a1046c
Dennis <dvdstelt@gmail.com>
2024-10-18 14:08:12 +0200

    Add new movie "Deadpool & Wolverine"
    Replace placeholder for "Jay and Silent Bob Reboot" with details about "Deadpool & Wolverine" including reviews. Removed older commented-out movie entries to clean up the codebase.

--- src/Shared/Configuration/DefaultData.cs
+++ src/Shared/Configuration/DefaultData.cs
@@ -29,22 +29,22 @@ namespace Shared.Configuration
                     PopularityScore = 1000,
                     TicketType = TicketType.DrawingTicket,
                 },
-                //new Movie
-                //{
-                //    Id = Guid.NewGuid(),
-                //    UrlTitle = "jayandsilentbobreboot",
-                //    Title = "Jay and Silent Bob Reboot",
-                //    Image = "jayandsilentbobreboot.jpg",
-                //    Rating = 2,
-                //    Description =
-                //        "Jay and Silent Bob return to Hollywood to stop a reboot of &#39;Bluntman and Chronic&#39; movie from getting made.",
-                //    Icons = new List<string> {"16", "alcoholdrugabuse", "explicitlanguage"},
-                //    MovieDetails = "105 minutes | English, Dutch subtitles",
-                //    ReleaseDate = new DateTime(2019, 10, 15),
-                //    Showtimes = new List<string> {"10:00", "15:00", "20:00"},
-                //    PricePerTicket = 10D,
-                //    PopularityScore = 200,
-                //},
+                new Movie
+                {
+                    Id = Guid.NewGuid(),
+                    UrlTitle = "deadpool",
+                    Title = "Deadpool & Wolverine",
+                    Image = "deadpool.jpg",
+                    Rating = 4,
+                    Description =
+                        "Deadpool is offered a place in the Marvel Cinematic Universe by the Time Variance Authority, but instead recruits a variant of Wolverine to save his universe from extinction.",
+                    Icons = new List<string> {"16", "violence", "explicitlanguage"},
+                    MovieDetails = "127 minutes | English, Dutch subtitles",
+                    ReleaseDate = new DateTime(2024, 7, 24),
+                    Showtimes = new List<string> {"10:00", "15:00", "20:00"},
+                    PricePerTicket = 10D,
+                    PopularityScore = 200,
+                },
                 new Movie
                 {
                     Id = Guid.NewGuid(),
@@ -61,61 +61,23 @@ namespace Shared.Configuration
                     PricePerTicket = 20D,
                     PopularityScore = 500,
                 },
-                new Movie
-                {
-                    Id = Guid.NewGuid(),
-                    UrlTitle = "freeguy",
-                    Title = "FreeGuy",
-                    Image = "freeguy.jpg",
-                    Rating = 5,
-                    Description = "A bank teller discovers that he's actually an NPC inside a brutal, open world video game.",
-                    Icons = new List<string> {"12", "violence", "explicitlanguage" },
-                    MovieDetails = "115 minutes | English, Dutch subtitles",
-                    ReleaseDate = new DateTime(2021,08,11),
-                    Showtimes = new List<string> { "15:00", "20:00", "23:00" },
-                    PricePerTicket = 10D,
-                    PopularityScore = 250
-                }
-                // new Movie
-                // {
-                //     Id = Guid.NewGuid(),
-                //     UrlTitle = "maverick",
-                //     Title = "Top Gun Maverick",
-                //     Image = "maverick.jpg",
-                //     Rating = 5,
-                //     Description = "After thirty years, Maverick is still pushing the envelope as a top naval aviator, but must confront ghosts of his past when he leads TOP GUN's elite graduates on a mission that demands the ultimate sacrifice from those chosen to fly it.",
-                //     Icons = new List<string> {"12", "violence", "explicitlanguage" },
-                //     MovieDetails = "2 hours 10 minutes | English, Dutch subtitles",
-                //     ReleaseDate = new DateTime(2022,04,27),
-                //     Showtimes = new List<string> { "15:00", "20:00", "23:00" },
-                //     PricePerTicket = 10D,
-                //     PopularityScore = 250
-                // }
             };
         }
 
         public static List<Review> GetDefaultReviews(IEnumerable<Movie> movies)
         {
             var gotId = movies.Single(s => s.UrlTitle == "gameofthrones").Id;
-            //var jsbId = movies.Single(s => s.UrlTitle == "jayandsilentbobreboot").Id;
-            // var tros = movies.Single(s => s.UrlTitle == "riseofskywalker").Id;
-            // var tenet = movies.Single(s => s.UrlTitle == "tenet").Id;
+            var deadpoolId = movies.Single(s => s.UrlTitle == "deadpool").Id;
             var dune = movies.Single(s => s.UrlTitle == "dune").Id;
-            var freeguy = movies.Single(s => s.UrlTitle == "freeguy").Id;
-            // var maverick = movies.Single(s => s.UrlTitle == "maverick").Id;
 
             return new List<Review>
             {
                 new Review() { Id = Guid.NewGuid(), MovieIdentifier = gotId, Description = "A riddle: A father and his son fight in the Battle of the Bastards. The father is killed in battle and the son is brought to the Warden of the North to be knighted. The Warden of the North looks at the boy and says: \"I can't knight him, he's my son!\" How can this be?", ReviewedAt = new DateTime(2019, 05, 19, 23, 30, 12) },
                 new Review() { Id = Guid.NewGuid(), MovieIdentifier = gotId, Description = "I want more Arya! She's the best!", ReviewedAt = new DateTime(2019, 05, 19, 23, 35, 36) },
-                //new Review() { Id = Guid.NewGuid(), MovieIdentifier = jsbId, Description = "Kevin Smith is my favorite director and this movie is another great piece of work.", ReviewedAt = new DateTime(2019, 10, 8, 12, 12, 12) },
-                //new Review() { Id = Guid.NewGuid(), MovieIdentifier = jsbId, Description = "Snootch to the nootch!!!", ReviewedAt = new DateTime(2019, 10, 9, 13, 45, 18) },
+                new Review() { Id = Guid.NewGuid(), MovieIdentifier = deadpoolId, Description = "Ryan Reynolds is hilarious again! Might just be the best Deadpool yet!", ReviewedAt = new DateTime(2024, 7, 25, 12, 12, 12) },
+                new Review() { Id = Guid.NewGuid(), MovieIdentifier = deadpoolId, Description = "Loved it! Don't forget to wait for the past-credits scene! Never laughed so hard.", ReviewedAt = new DateTime(2025, 7, 26, 13, 45, 18) },
                 new Review() { Id = Guid.NewGuid(), MovieIdentifier = dune, Description = "Great visuals, screenplay adaptation is too close to the books though.", ReviewedAt = new DateTime(2021, 10, 8, 8, 14, 56) },
                 new Review() { Id = Guid.NewGuid(), MovieIdentifier = dune, Description = "Can't wait to go back to Mordor!", ReviewedAt = new DateTime(2021, 10, 9, 13, 37, 00) },
-                new Review() { Id = Guid.NewGuid(), MovieIdentifier = freeguy, Description = "Reynolds is again too funny", ReviewedAt = new DateTime(2021, 10, 8, 8, 14, 56) },
-                new Review() { Id = Guid.NewGuid(), MovieIdentifier = freeguy, Description = "When is Deadpool vs Wolverine coming out!?", ReviewedAt = new DateTime(2021, 10, 9, 13, 42, 00) },
-                // new Review() { Id = Guid.NewGuid(), MovieIdentifier = maverick, Description = "This movie needs to been in IMAX! That sound is incredible!", ReviewedAt = new DateTime(2022, 09, 21, 12, 12, 12) },
-                // new Review() { Id = Guid.NewGuid(), MovieIdentifier = maverick, Description = "Maverick is so much better than the original!", ReviewedAt = new DateTime(2022, 09, 20, 13, 45, 18) },
             };
         }
 
